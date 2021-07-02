209a19daf5c9081dbe4f480d92313a1e6964bfd6
EC2 Default User <ec2-user@ip-172-31-93-9.ec2.internal>
2021-07-02 08:38:25 +0000

    Delete jQuery

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '97334394734a64f13212254e2e6b8c2e49dca266cc3d021a28f7c51eeaf4f29ee07c1c7af2437ffb0ce7dddc0907ace678bb4198edefdb1473a80f0b34285760'
