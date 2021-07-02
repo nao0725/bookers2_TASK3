bc6f2c7ae48e45a41a1cd5b84e8eb3de8a3bd22e
EC2 Default User <ec2-user@ip-172-31-93-9.ec2.internal>
2021-07-02 08:50:28 +0000

    Add to asociate

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:index,:show,:edit,:update]
+  resource :favorites, only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_043835) do
+ActiveRecord::Schema.define(version: 2021_07_02_084313) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "image_id"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
